f4c2ff4de6557f15c842e80c672f3366ab5053be
Luigi-Crisci <luigicrisci1997@gmail.com>
2023-03-10 19:06:46 +0100

    Struct_accessor added

--- include/empi/layouts.hpp
+++ include/empi/layouts.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_EMPI_LAYOUTS
 
 #include "empi/datatype.hpp"
+#include <functional>
 #include <memory>
 
 namespace empi{
@@ -27,7 +28,7 @@ namespace layouts{
 		
 		//Hardwritten for 2D layouts, will change later...
 		template<template<typename, size_t...> typename Extents, typename K, size_t ...Idx>
-		static auto build(std::ranges::forward_range auto&& view, Extents<K,Idx...> extents, const int col){
+		static auto build(std::ranges::forward_range auto&& view, Extents<K,Idx...> extents, size_t col){
 			static_assert(Extents<K,Idx...>::rank() == 2);
 			assert(col < extents.extent(1));
 			using extent_type = decltype(remove_last(extents));
@@ -44,7 +45,7 @@ namespace layouts{
 
 		template<typename T, typename Extents>
 		static std::unique_ptr<T[]>&& compact(const column_view<T,Extents>& view){
-			stdex::mdspan<int, stdex::dextents<size_t, 1>,stdex::layout_stride> x;
+			
 			auto ptr = std::make_unique<T[]>(view.extent(0));
 			for (size_t i = 0; i < view.extent(0); ++i)
 				ptr[i] = view(i);
@@ -57,19 +58,18 @@ namespace layouts{
 			using base = stdex::layout_stride::mapping<Extents>;
 			using base::base;
 		};
+
 	};
 
 	struct contiguous_layout{
 		// Wraps data into plain, 1D mdspan.
 		// Used for implicit conversions
-		template<template<typename, size_t...> typename Extents, typename K, size_t ...Idx>
 		static auto build(std::ranges::forward_range auto&& view){
 			using extent_type = stdex::dextents<std::size_t, 1>;
 			extent_type extents(std::ranges::size(view));
 			using T = std::ranges::range_value_t<decltype(view)>;
-			return stdex::mdspan<T, extent_type, std::experimental::layout_right>(
-				std::ranges::data(view),
-				extents);
+			return std::move(stdex::mdspan<T, extent_type, std::experimental::layout_right>(std::ranges::data(view),
+																							extents));
 		}
 
 		template<typename Extents>
@@ -79,7 +79,31 @@ namespace layouts{
 		};
 	};
 
+	struct struct_layout {
 
+		template<typename Element_type>
+		static constexpr auto default_access = [](Element_type& value) -> Element_type& {return value;};
+
+		template<typename Element_type, typename Callable = std::remove_cv_t<decltype(default_access<Element_type>)> >
+		struct struct_accessor{
+
+			using offset_policy = stdex::default_accessor<Element_type>;
+			using element_type = function_traits<Callable>::result_type;
+			using reference = element_type&;
+			using data_handle_type = Element_type*;
+			
+			constexpr struct_accessor() : proj(default_access<Element_type>) {};
+			explicit struct_accessor(Callable&& c) : proj(c) {}
+
+			constexpr reference access(data_handle_type p, size_t i) const noexcept{
+					return proj(p[i]);
+			}
+
+			private:
+				Callable proj;
+		};
+			
+	};
 
 
 
--- test/test_layouts.cpp
+++ test/test_layouts.cpp
@@ -20,12 +20,12 @@ void print(const empi::stdex::mdspan<T, Extents>& view){
 		if(i != 0) std::cout << "\n";
 			std::cout <<"\t";
 		for(int j = 0; j < view.extent(1); ++j){
-			std::cout << view[i,j] << ", ";
+			std::cout << view(i,j) << ", ";
 		}
 	}
 }
 
-TEST_CASE("Create column mdspan", "[column_layouts]") {
+TEST_CASE("Create column mdspan", "[column_layout]") {
 	std::vector<int> v(10);
 	std::iota(v.begin(),v.end(),0);
 
@@ -34,7 +34,7 @@ TEST_CASE("Create column mdspan", "[column_layouts]") {
 	auto view = empi::layouts::column_layout::build(v, ext, 0);
 
 	REQUIRE(decltype(view)::rank() == 1);
-	REQUIRE(view.extent(0) == 5);
+	REQUIRE(view.extent(0) == 2);
 
 	for(int i = 1, j = 0; i < 10; i+=2,j++)
 		REQUIRE(v[i] == view[j]);
@@ -45,5 +45,23 @@ TEST_CASE("Check extents type traits", "[mdspan]") {
 	empi::stdex::extents<int, 1,2,3,4,5> ex;
 	using trimmed_extent = decltype(empi::remove_last(ex));
 	REQUIRE(std::is_same_v<trimmed_extent,empi::stdex::extents<int, 1,2,3,4>>);
+}
+
+struct S{
+	int x;
+	int y;
+};
+
+TEST_CASE("Create struct mdspan", "[mdspan|struct_layout]") {
+	std::vector<S> tmp(10);
+	int count = 0;
+	std::transform(tmp.begin(), tmp.end(), tmp.begin(),[&count](S& s){s.x=count++; s.y=count++;return s;});
+	auto proj = [](S& s) -> int& {return s.y;};
+    auto ext = empi::stdex::extents(10);
+    empi::stdex::mdspan view{tmp.data(), empi::stdex::layout_right::mapping{ext}, empi::layouts::struct_layout::struct_accessor<S,decltype(proj)>(std::move(proj))};
+
+	for(int i = 0; i < 10; i+=1){
+		REQUIRE(view[i] == i*2+1);
+	}
 }
 
