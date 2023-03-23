9f7a7009feab79e642e698ab2660618478efe714
Luigi-Crisci <luigicrisci1997@gmail.com>
2023-03-23 17:59:04 +0100

    Block layout added for experiments

--- include/empi/layouts.hpp
+++ include/empi/layouts.hpp
@@ -11,11 +11,6 @@
 
 namespace empi::layouts {
 
-template <typename T, typename Extents,
-          typename AccessPolicy = stdex::default_accessor<T>>
-using column_view =
-    stdex::mdspan<T, Extents, stdex::layout_stride, AccessPolicy>;
-
 struct column_layout {
 
   // Hardwritten for 2D layouts, will change later...
@@ -130,21 +125,24 @@ auto make_struct_accessor(Callable &&c) {
 struct tiled_layout {
 
   // Hardwritten for 2D layouts, will change later...
-  [[nodiscard]] static constexpr auto build(std::ranges::forward_range auto &&view, const size_t row, const size_t col) {
+  [[nodiscard]] static constexpr auto
+  build(std::ranges::forward_range auto &&view, const size_t row,
+        const size_t col) {
     return tiled_layout::build(
         view, stdex::dextents<size_t, 2>(row, col),
         stdex::default_accessor<std::ranges::range_value_t<decltype(view)>>());
   }
 
   // Hardwritten for 2D layouts, will change later...
-  template <template<typename,size_t...> typename Extents, typename T, size_t ...idx, typename Accessor>
-  [[nodiscard]] static constexpr auto build(std::ranges::forward_range auto &&view,
-                                  Extents<T,idx...> extents,
-                                  const Accessor &acc) {
-    //TODO: Check sizes against view 
-	//TODO: Tiled layout should work on every dimension
-	using extent_type = Extents<T,idx...>;
-	static_assert(extent_type::rank() == 2);
+  template <template <typename, size_t...> typename Extents, typename T,
+            size_t... idx, typename Accessor>
+  [[nodiscard]] static constexpr auto
+  build(std::ranges::forward_range auto &&view, Extents<T, idx...> extents,
+        const Accessor &acc) {
+    // TODO: Check sizes against view
+    // TODO: Tiled layout should work on every dimension
+    using extent_type = Extents<T, idx...>;
+    static_assert(extent_type::rank() == 2);
 
     tiled_layout_impl::mapping<extent_type> tiled_mapping(extents);
     using view_data_type = std::ranges::range_value_t<decltype(view)>;
@@ -268,6 +266,108 @@ struct tiled_layout {
       Extents extents_;
       size_type row_tile_size_ = 1;
       size_type col_tile_size_ = 1;
+    };
+  };
+};
+
+//////////// Layouts for benchmarking purposes ///////////////
+struct block_layout {
+
+  struct block_layout_impl {
+
+    template <typename Extents> struct mapping {
+
+      static_assert(Extents::rank() == 1,
+                    "Hardwritten for 1D layouts, for now");
+
+      using extents_type = Extents;
+      using rank_type = typename Extents::rank_type;
+      using size_type = typename Extents::size_type;
+      using layout_type = block_layout::block_layout_impl;
+
+      mapping() noexcept = default;
+      mapping(const mapping &) noexcept = default;
+      mapping &operator=(const mapping &) noexcept = default;
+
+      template <details::typed_range<size_t> K>
+      constexpr mapping(const Extents& ext, K &&b, K &&s)
+          : _extents(ext), blocks(b), strides(s), sum_blocks(0), sum_diffs(0)
+      {
+        // At least one block and stride
+        for (int i = 0; i < blocks.size(); i++) {
+          diffs.push_back(strides[i] - blocks[i]);
+          sum_blocks += blocks[i];
+          sum_diffs += diffs[i];
+        }
+
+        offsets.resize(diffs.size() - 1);
+        std::inclusive_scan(diffs.begin(), diffs.end() -1 ,offsets.begin());
+
+        partial_blocks.resize(blocks.size());
+        std::inclusive_scan(blocks.begin(), blocks.end() ,partial_blocks.begin());
+      }
+
+      // TODO: copy constructor
+
+      // Mandatory member methods
+
+      constexpr const extents_type &extents() const { return _extents; }
+
+      constexpr size_type required_span_size() const noexcept {
+        // return n_row_tiles() * n_column_tiles() * tile_size();
+      }
+
+      template<class index>
+      constexpr inline size_t pos_in_blocks(index idx) const noexcept {
+        return idx / sum_blocks;
+      }
+
+      template<class index>
+      constexpr inline size_t offset_in_block(index idx) const noexcept {
+        const auto pos = idx % sum_blocks;
+        if(pos <= blocks[0])
+            return 0;
+        
+        const auto new_pos = std::distance(partial_blocks.begin(),
+                                            std::find_if(partial_blocks.begin(), partial_blocks.end(), [pos](auto& e){return pos < e;})) - 1;
+        return offsets[new_pos];
+      }
+
+      template <class index>
+      constexpr size_type operator()(index idx) const noexcept {
+        return idx + sum_diffs * pos_in_blocks(idx) + offset_in_block(idx);  
+      }
+
+      // Mapping is always unique
+      static constexpr bool is_always_unique() noexcept { return true; }
+      // Only exhaustive if extents_.extent(0) % column_tile_size == 0, so not
+      // always
+      static constexpr bool is_always_exhaustive() noexcept { return false; }
+      // There is not always a regular stride between elements in a given
+      // dimension
+      static constexpr bool is_always_strided() noexcept { return false; }
+
+      static constexpr bool is_unique() noexcept { return true; }
+      constexpr bool is_exhaustive() const noexcept {
+        // Only exhaustive if extents fit exactly into tile sizes...
+        // return (extents_.extent(0) % row_tile_size_ == 0) &&
+              //  (extents_.extent(1) % col_tile_size_ == 0);
+        return false; //TODO....
+      }
+      // There are some circumstances where this is strided, but we're not
+      // concerned about that optimization, so we're allowed to just return
+      // false here
+      constexpr bool is_strided() const noexcept { return true; }
+
+    private:
+      Extents _extents;
+      std::vector<size_t> blocks;
+      std::vector<size_t> partial_blocks;
+      std::vector<size_t> strides;
+      std::vector<size_t> diffs;
+      std::vector<size_t> offsets;
+      size_t sum_diffs{};
+      size_t sum_blocks{};
     };
   };
 };
