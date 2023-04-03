73685652775eb290ff8db1dd87db8f1fc0fe849d
Luigi-Crisci <luigicrisci1997@gmail.com>
2023-04-03 10:53:55 +0200

    Datatype script finalization

--- scripts/common.py
+++ scripts/common.py
@@ -58,24 +58,29 @@ def run_experiment(args,exp_name,run_command, parse_output):
     print("-----------------------------------------------")
     
 def make_minibench_command(args, exp_path):
-	return [
-		f'{args.ompi_path}bin/mpirun',
-		f'--allow-run-as-root',
-		f'{"--oversubscribe" if multiprocessing.cpu_count() < args.num_proc else "--nooversubscribe"}',
-		'-n',
-		f'{args.num_proc}',
-		f'{args.bench_path}{exp_path}',  # App name
-		f'{args.size}',
-		f'{args.app_iter}'
+    return [
+        f'{args.ompi_path}bin/mpirun',
+        f'--allow-run-as-root',
+        f'{"--oversubscribe" if multiprocessing.cpu_count() < args.num_proc else "--nooversubscribe"}',
+        '-n',
+        f'{args.num_proc}',
+        f'{args.bench_path}{exp_path}',  # App name
+        f'{args.size}',
+        f'{args.app_iter}'
 ]
+
+def make_datatype_command(args, exp_path, data_sizes):
+    base = make_minibench_command(args, exp_path)
+    base.extend(data_sizes)
+    return base
 
 def make_lulesh_command(args):
     return [
         f'{args.ompi_path}bin/mpirun',
-	    '--allow-run-as-root',
-	    f'{"--oversubscribe" if multiprocessing.cpu_count() < args.num_proc else "--nooversubscribe"}',
-	    '-n',
-	    f'{args.num_proc}',
+        '--allow-run-as-root',
+        f'{"--oversubscribe" if multiprocessing.cpu_count() < args.num_proc else "--nooversubscribe"}',
+        '-n',
+        f'{args.num_proc}',
         f'{args.lulesh_path}lulesh2.0',  # App name
         '-s',
         f'{args.size}',
@@ -93,31 +98,31 @@ def parse_lulesh(param):
                 return match.group()    
 
 def exp_full(args, exp_name, exp_path, op):
-	num_procs = [2,4,8,16,32]
-	sizes = [2,4,8,10,12,14,16]
-	for num_proc in num_procs:
-		for size in sizes:
-			args.num_proc = num_proc
-			args.size = size
-			run_experiment(args, exp_name, make_minibench_command(args, exp_path),op)
+    num_procs = [2,4,8,16,32]
+    sizes = [2,4,8,10,12,14,16]
+    for num_proc in num_procs:
+        for size in sizes:
+            args.num_proc = num_proc
+            args.size = size
+            run_experiment(args, exp_name, make_minibench_command(args, exp_path),op)
 
 def exp_fixed_proc(args, exp_name, exp_path, num_proc, op):
-	sizes = [2,4,8,10,12,14,16]
-	args.num_proc = num_proc
-	for size in sizes:
-		args.size = size
-		run_experiment(args, exp_name, make_minibench_command(args, exp_path),op)
+    sizes = [2,4,8,10,12,14,16]
+    args.num_proc = num_proc
+    for size in sizes:
+        args.size = size
+        run_experiment(args, exp_name, make_minibench_command(args, exp_path),op)
 
 def exp_fixed_size(args, exp_name, exp_path, size, op):
-	num_procs = [2,4,8,16,32]
-	args.size = size
-	for proc in num_procs:
-		args.num_proc = proc
-		run_experiment(args, exp_name, make_minibench_command(args, exp_path),op)
+    num_procs = [2,4,8,16,32]
+    args.size = size
+    for proc in num_procs:
+        args.num_proc = proc
+        run_experiment(args, exp_name, make_minibench_command(args, exp_path),op)
 
 def exp_lulesh(args, exp_name, size, op):
-	num_procs = [1, 8, 64, 216, 512, 729, 1000]
-	args.size = 1
-	for proc in num_procs:
-		args.num_proc = proc
-		run_experiment(args, exp_name, make_lulesh_command(args),op)
+    num_procs = [1, 8, 64, 216, 512, 729, 1000]
+    args.size = 1
+    for proc in num_procs:
+        args.num_proc = proc
+        run_experiment(args, exp_name, make_lulesh_command(args),op)
--- scripts/datatype.py
+++ scripts/datatype.py
@@ -1,58 +1,87 @@
 #!/usr/bin/python3
 import common
 from common import run_experiment
-from common import make_minibench_command
+from common import make_datatype_command
 
 if __name__ == '__main__':
-	p = common.base_args()
-	p = common.common_args(p)
-	p.add_argument("--bench_path",
-                 type=str,
-                 default="../build/examples/datatype",
-                 help="Path to the benchmark folder (default: ../build/examples)"
-                 )
-	p.add_argument("--tiled_sizes",
-                 type=int,
-                 nargs=2,
-                 default=[1,1],
-                 help="Tiled layout sizes (default: A=1, B=1)"
-                 )
-	p.add_argument("--bucket_sizes",
-                 type=int,
-                 nargs=3,
-                 default=[1,1,1],
-                 help="Bucket layout sizes (default: A1=1, A2=1, B=1)"
-                 )
-	p.add_argument("--block_sizes",
-                 type=int,
-                 nargs=3,
-                 default=[1,1,1],
-                 help="Block layout sizes (default: A=1, B1=1, B2=1)"
-                 )
-	p.add_argument("--alternating_sizes",
-                 type=int,
-                 nargs=4,
-                 default=[1,1,1,1],
-                 help="Alternating layout sizes (default: A1=1, A2=1, B1=1, B2=1)"
-                 )
-	args = p.parse_args()
-	noop= lambda x: x
-	layouts = ["tiled", "block", "bucket", "alternating"]
- 
-	tmp = args.num_proc
-	args.num_proc = 2
-	run_experiment(args, "Ping pong: MPI", make_minibench_command(args, "ping_pong/mpi_ping_pong"),noop)
-	run_experiment(args, "Ping_pong: EMPI", make_minibench_command(args, "ping_pong/empi_ping_pong"),noop)
-	args.num_proc = tmp
+    p = common.base_args()
+    p = common.common_args(p)
+    p.add_argument("--bench_path",
+                   type=str,
+                   default="../build/examples/datatypes/",
+                   help="Path to the benchmark folder (default: ../build/examples/datatypes/)"
+                   )
+    p.add_argument("--tiled",
+                   action='store_true',
+                   default=False,
+                   help="Enable tiled benchs"
+                   )
+    p.add_argument("--tiled_sizes",
+                   type=int,
+                   nargs=2,
+                   help="Tiled layout sizes (default: A=1, B=1)"
+                   )
+    p.add_argument("--bucket",
+                   action='store_true',
+                   default=False,
+                   help="Enable bucket benchs"
+                   )
+    p.add_argument("--bucket_sizes",
+                   type=int,
+                   nargs=3,
+                   help="Bucket layout sizes (default: A1=1, A2=1, B=1)"
+                   )
+    p.add_argument("--block",
+                   action='store_true',
+                   default=False,
+                   help="Enable block benchs"
+                   )
+    p.add_argument("--block_sizes",
+                   type=int,
+                   nargs=3,
+                   help="Block layout sizes (default: A=1, B1=1, B2=1)"
+                   )
+    p.add_argument("--alternating",
+                   action='store_true',
+                   default=False,
+                   help="Enable alternating benchs"
+                   )
+    p.add_argument("--alternating_sizes",
+                   type=int,
+                   nargs=4,
+                   help="Alternating layout sizes (default: A1=1, A2=1, B1=1, B2=1)"
+                   )
+    args = p.parse_args()
+    def noop(x): return x
 
-	run_experiment(args, "Bidirectional ring: MPI", make_minibench_command(args, "bdring/mpi_bdring"),noop)
-	run_experiment(args, "Bidirectional ring: EMPI", make_minibench_command(args, "bdring/empi_bdring"),noop)
+    layouts = []
+    layout_args = dict()
+    if args.tiled and (args.tiled_sizes is not None):
+        layouts.append("tiled")
+        layout_args["tiled"] = [str(x) for x in args.tiled_sizes]
+    if args.bucket and (args.bucket_sizes is not None):
+        layouts.append("bucket")
+        layout_args["bucket"] = [str(x) for x in args.bucket_sizes]
+    if args.block and (args.block_sizes is not None):
+        layouts.append("block")
+        layout_args["block"] = [str(x) for x in args.block_sizes]
+    if args.alternating and (args.alternating_sizes is not None):
+        layouts.append("alternating")
+        layout_args["alternating"] = [str(x) for x in args.alternating_sizes]
 
-	run_experiment(args, "Allreduce: MPI", make_minibench_command(args, "all_reduce/mpi_allreduce"),noop)
-	run_experiment(args, "Allreduce: EMPI", make_minibench_command(args, "all_reduce/empi_allreduce"),noop)
+    print(f"Conf: {args}")
 
-	run_experiment(args, "IBcast: MPI", make_minibench_command(args, "ibcast/mpi_ibcast"),noop)
-	run_experiment(args, "IBcast: EMPI", make_minibench_command(args, "ibcast/empi_ibcast"),noop)
-
-	run_experiment(args, "Bcast: MPI", make_minibench_command(args, "bcast/mpi_bcast"),noop)
-	run_experiment(args, "Bcast: EMPI", make_minibench_command(args, "bcast/empi_bcast"),noop)
+    for layout in layouts:
+        print(f"//--------------- {layout} layout -----------------//")
+        tmp = args.num_proc
+        args.num_proc = 2
+        run_experiment(args, f"{layout} -> Ping pong: MPI", make_datatype_command(args, f"{layout}/{layout}_mpi_ping_pong", layout_args[layout]),noop)
+        run_experiment(args, f"{layout} -> Ping_pong: EMPI", make_datatype_command(args, f"{layout}/{layout}_empi_ping_pong", layout_args[layout]),noop)
+        args.num_proc = tmp
+        
+        run_experiment(args, f"{layout} -> Bcast: MPI", make_datatype_command(args, f"{layout}/{layout}_mpi_bcast", layout_args[layout]),noop)
+        run_experiment(args, f"{layout} -> Bcast: EMPI", make_datatype_command(args, f"{layout}/{layout}_empi_bcast", layout_args[layout]),noop)
+        
+        run_experiment(args, f"{layout} -> Allgather: MPI", make_datatype_command(args, f"{layout}/{layout}_mpi_allgather", layout_args[layout]),noop)
+        run_experiment(args, f"{layout} -> Allgather: EMPI", make_datatype_command(args, f"{layout}/{layout}_empi_allgather", layout_args[layout]),noop)
+        print(f"//---------------------------------------------------//")
